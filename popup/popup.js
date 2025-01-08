dc62f196f7cbf66d3c66dc97f95e525ab7accb78
perevozniuk13 <sashaperevozniuk01@gmail.com>
2025-01-08 13:36:28 +0000

    add logic to save notes to local storage

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 console.log("This is a popup!")
+
+const currentNote = document.getElementById('note');
+console.log(currentNote)
+
+
+currentNote.oninput = function saveNote() {
+    chrome.storage.local.set({ note: currentNote.value });
+}
+
+chrome.storage.local.get('note', (data) => {
+    if (data.note) {
+        currentNote.value = data.note; 
+    }
+});
+
+})
+
+
+
+  
